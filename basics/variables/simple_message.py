5d356eaced102354d776d866d60e32d60545ec1b
MoJallow <97028931+MoJallow@users.noreply.github.com>
2024-04-21 00:18:02 +0300

    Part I: Basics

---
+++ basics/variables/simple_message.py
@@ -0,0 +1,6 @@
+# Assign a value to the variable
+message = "Greetings, Python learner!"
+print(message)
+
+# Think of variables as labels that you can always assign to values.
+# You can always say that a variable references a certain value.
---
+++ basics/variables/simple_messages.py
@@ -0,0 +1,10 @@
+# Assign a value to the variable
+message = "Greetings, Python learner!"
+print(message)
+
+# Assign a new value to the variable
+message = "Welcome to Python Crash Course."
+print(message)
+
+# You can change the value of a variable in your program at any time, and Python 
+# will keep track of its current value
